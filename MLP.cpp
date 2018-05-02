2cbd575922e4de829fd7959c9b4e910aeba048b4
Julius Montvydas <montvydas.julius@gmail.com>
2018-05-02 13:01:44 +0300

    Revert "fixed code smell in: KMEANS, MLP, SAMANN."
    This reverts commit 814c7d24baedfce7c6ab2f1355a04a09b3bbcb58.

--- MLP.cpp
+++ MLP.cpp
@@ -9,30 +9,30 @@ MLP::MLP(int h1pNo, int h2pNo, double qtty, int maxIter, bool validationMethod)
     //no of neurons in hidden layer
     h1No = h1pNo;
     h2No = h2pNo;
- //   param = qtty;
-   // this->dL = dL; //learning
-   // this->dT = dT; //testing
- //   this->decay = wDecay;
+    //   param = qtty;
+    // this->dL = dL; //learning
+    // this->dT = dT; //testing
+    //   this->decay = wDecay;
     this->maxIter = maxIter;
     this->kFoldValidation = validationMethod;
 
-    alglib::mlpcreatetrainercls(X.getObjectAt(0).getFeatureCount(), X.getClassCount(), trn); 
-	//qtt of input features, number of classes to be produced
+    alglib::mlpcreatetrainercls(X.getObjectAt(0).getFeatureCount(), X.getClassCount(), trn);
+    //qtt of input features, number of classes to be produced
 
     double wstep = 0.000;
     mlpsetdecay(trn, 0.001); // by default we set moderate weight decay
-    mlpsetcond(trn, wstep, this->maxIter);     // * we choose iterations limit as stopping condition 
-											   //(another condition - step size - is zero, which means than this condition is not active)
+    mlpsetcond(trn, wstep, this->maxIter);     // * we choose iterations limit as stopping condition
+    //(another condition - step size - is zero, which means than this condition is not active)
 
     if ((h1No > 0) && (h2No > 0))
-        alglib::mlpcreatec2(X.getObjectAt(0).getFeatureCount(), h1No, h2No, X.getClassCount(), network); 
-	//create nn network with noofinput features, 2 hidden layers, noofclasses (and sore to network variable)
+        alglib::mlpcreatec2(X.getObjectAt(0).getFeatureCount(), h1No, h2No, X.getClassCount(), network);
+    //create nn network with noofinput features, 2 hidden layers, noofclasses (and sore to network variable)
     if ((h1No > 0) && (h2No == 0))
-         alglib::mlpcreatec1(X.getObjectAt(0).getFeatureCount(), h1No, X.getClassCount(), network); 
-	//create nn network with no of input features, 1 hidden layer, noofclasses (and sore to network variable)
+        alglib::mlpcreatec1(X.getObjectAt(0).getFeatureCount(), h1No, X.getClassCount(), network);
+    //create nn network with no of input features, 1 hidden layer, noofclasses (and sore to network variable)
     if ((h1No == 0) && (h2No == 0))
-        alglib::mlpcreatec0(X.getObjectAt(0).getFeatureCount(), X.getClassCount(), network); 
-	//create nn network with no of input features, 0 hidden layer, noofclasses (and sore to network variable)
+        alglib::mlpcreatec0(X.getObjectAt(0).getFeatureCount(), X.getClassCount(), network);
+    //create nn network with no of input features, 0 hidden layer, noofclasses (and sore to network variable)
 ///h2No must be non zero
 
     if (this->kFoldValidation == true) //do kfold validation
@@ -49,9 +49,9 @@ MLP::MLP(int h1pNo, int h2pNo, double qtty, int maxIter, bool validationMethod)
 
         alglib::mlpsetdataset(trn, ClusterizationMethods::learnSet, ClusterizationMethods::learnObjQtty); //attach learning data to data set
 
-        alglib::mlptrainnetwork(trn, network, 1, rep); 
-		// train network NRestarts=1, network is trained from random initial state. 
-		//With NRestarts=0, network is trained without randomization (original state is used as initial point).
+        alglib::mlptrainnetwork(trn, network, 1, rep);
+        // train network NRestarts=1, network is trained from random initial state.
+        //With NRestarts=0, network is trained without randomization (original state is used as initial point).
         alglib::integer_1d_array Subset;
         Subset.setlength(10);
         alglib::mlpallerrorssubset(network, testSet, testObjQtty, Subset, -1, repp);
@@ -111,14 +111,14 @@ ObjectMatrix MLP::getProjection()
             Y.updateDataObjectClass(i, indx);
     }
 
-        std::vector <std::string > probabilities; probabilities.reserve(0);
+    std::vector <std::string > probabilities; probabilities.reserve(0);
 
-        for (int i = 0; i < X.getClassCount(); i++)
-            probabilities.push_back("probClass" + X.getStringClassAttributes().at(i));
+    for (int i = 0; i < X.getClassCount(); i++)
+        probabilities.push_back("probClass" + X.getStringClassAttributes().at(i));
 
-        Y.addAtributes(probabilities);
+    Y.addAtributes(probabilities);
 
-        Y.setPrintClass(X.getStringClassAttributes());
+    Y.setPrintClass(X.getStringClassAttributes());
 
     return Y;
 }
@@ -141,5 +141,5 @@ double MLP::getStress()
     * Rep.AvgError - average error
     * Rep.AvgRelError - average relative error
     */
-  return rep.rmserror;
+    return rep.rmserror;
 }
